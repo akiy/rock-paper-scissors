de879eed05a51efdf97c143a9eb66189c06e0dc2
akira <rafael.akkira@gmail.com>
2021-11-20 11:41:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
 let gameResult = document.querySelector('.game-result');
 
 
-function playGame() {
-	let playerChoice = getPlayerChoice();
+function playGame(el) {
+	let playerChoice = el.target.textContent.trim().toLowerCase();
 
 	if (playerChoice) {
 		let computerChoice  = getComputerChoice();
 		console.log(playerChoice, computerChoice);
 		showMatchResult(playerChoice, computerChoice);
-	} else {
-		gameResult.innerHTML = "Choose your play!";
 	}
 }
 
 function showMatchResult(playerChoice, computerChoice) {
 
 	if (playerChoice == "rock" && computerChoice == "scissor") {
-		gameResult.innerHTML = "Player win!";
+		gameResult.innerHTML = "<p>Player win!</p>";
 	}
 
 	if (playerChoice == "rock" && computerChoice == "paper") {
-		gameResult.innerHTML = "Computer win!";
+		gameResult.innerHTML = "<p>You lose!</p>";
 	}
 
 	if (playerChoice == "paper" && computerChoice == "scissor") {
-		gameResult.innerHTML = "Computer win!";
+		gameResult.innerHTML = "<p>You lose!</p>";
 	}
 
 	if (playerChoice == "paper" && computerChoice == "rock") {
-		gameResult.innerHTML = "Player win!";
+		gameResult.innerHTML = "<p>Player win!</p>";
 	}
 
 	if (playerChoice == "scissor" && computerChoice == "rock") {
-		gameResult.innerHTML = "Computer win!";
+		gameResult.innerHTML = "<p>You lose!</p>";
 	}
 
 	if (playerChoice == "scissor" && computerChoice == "paper") {
-		gameResult.innerHTML = "Player win!";
+		gameResult.innerHTML = "<p>Player win!</p>";
 	}
 
 	if (playerChoice == computerChoice) {
-		gameResult.innerHTML = "Tie!";	
+		gameResult.innerHTML = "<p>Tie!</p>";	
 	}
-
-}
-
-function getPlayerChoice(el) {
-	console.log(el.target.textContent.trim().toLowerCase());
 }
 
 function getComputerChoice() {
@@ -55,6 +48,6 @@ function getComputerChoice() {
 	return choices[Math.floor(Math.random() * 3)];
 }
 
-document.querySelector('.rock').addEventListener('click', getPlayerChoice);
-document.querySelector('.paper').addEventListener('click', getPlayerChoice);;
-document.querySelector('.scissor').addEventListener('click', getPlayerChoice);;
+document.querySelector('.rock').addEventListener('click', playGame);
+document.querySelector('.paper').addEventListener('click', playGame);;
+document.querySelector('.scissor').addEventListener('click', playGame);;
